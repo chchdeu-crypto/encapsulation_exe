1d8f087eb8ea77f342083cca4fafc26f84c24acf
chaim <chchdeu@gmail.com>
2026-07-15 13:04:10 +0300

    mission_2

--- Encapsulatio_exe.py
+++ Encapsulatio_exe.py
@@ -7,6 +7,23 @@ class UserProfile:
         return self.__username
 chaim=UserProfile("chaim")
 print(chaim.username)
-print(chaim.__username)
+#print(chaim.__username)
+
+#mission 2
+class UserProfile:
+    def __init__(self,username, email):
+        self.__username=username
+        self.__email=email
+    @property
+    def username(self):
+        return self.__username
+    @property
+    def email(self):
+        return self.__email
+bob=UserProfile("bob","bob@mail.com")
+print(bob.username,bob.email)
+    
+        
+
 
         
